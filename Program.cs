bbd29295288462676d57485d18b9d98a59e94036
jbowen999 <julian.e.bowen@outlook.com>
2024-07-29 15:06:22 -0700

    File created on Mokaroo.com

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@
 
         static void Main()
         {
-            LoadData(@"");
+            LoadData("MOCK_DATA.csv");
             bool exit = false;
 
             while (!exit)
@@ -25,8 +25,11 @@
                         SearchUser();
                         break;
                     case "4":
-                        SaveData(@"");
+                        SaveData("MOCK_DATA.csv");
                         exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Please try again.");
                         break;
                 }
             }
@@ -34,45 +37,90 @@
 
         static void LoadData(string filePath)
         {
-            using (StreamReader sr = new StreamReader(filePath))
+            try
             {
-                sr.ReadLine(); // Skip header
-                while (!sr.EndOfStream)
+                using (StreamReader sr = new StreamReader(filePath))
                 {
-                    var line = sr.ReadLine();
-                    var data = line.Split(',');
-                    var user = new User(data[0], data[1], data[2], data[3]);
-                    userTree.Add(data[3], user); // Key is phone number
+                    sr.ReadLine(); // Skip header
+                    while (!sr.EndOfStream)
+                    {
+                        var line = sr.ReadLine();
+                        var data = line.Split(',');
+                        var user = new User(data[0], data[1], data[2], data[3]);
+                        userTree.Add(data[3], user); // Key is phone number
+                    }
                 }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error loading data: {e.Message}");
             }
         }
 
         static void SaveData(string filePath)
         {
-            using (StreamWriter sw = new StreamWriter(filePath))
+            try
             {
-                sw.WriteLine("last_name,first_name,email,phone");
-                foreach (var user in userTree.Values)
+                using (StreamWriter sw = new StreamWriter(filePath))
                 {
-                    sw.WriteLine($"{user.LastName},{user.FirstName},{user.Email},{user.Phone}");
+                    sw.WriteLine("last_name,first_name,email,phone");
+                    foreach (var user in userTree.Values)
+                    {
+                        sw.WriteLine($"{user.LastName},{user.FirstName},{user.Email},{user.Phone}");
+                    }
                 }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error saving data: {e.Message}");
             }
         }
 
         static void AddUser()
         {
-            Console.WriteLine("Enter last name:");
-            string lastName = Console.ReadLine();
-            Console.WriteLine("Enter first name:");
-            string firstName = Console.ReadLine();
-            Console.WriteLine("Enter email:");
-            string email = Console.ReadLine();
-            Console.WriteLine("Enter phone:");
-            string phone = Console.ReadLine();
+            try
+            {
+                Console.WriteLine("Enter last name:");
+                string lastName = Console.ReadLine();
+                Console.WriteLine("Enter first name:");
+                string firstName = Console.ReadLine();
+                Console.WriteLine("Enter email:");
+                string email = Console.ReadLine();
+                Console.WriteLine("Enter phone:");
+                string phone = Console.ReadLine();
 
-            var user = new User(lastName, firstName, email, phone);
-            userTree.Add(phone, user);
-            Console.WriteLine("User added.");
+                if (!User.ValidateName(lastName) || !User.ValidateName(firstName))
+                {
+                    Console.WriteLine("Invalid name. Please enter a valid name.");
+                    return;
+                }
+
+                if (!User.ValidateEmail(email))
+                {
+                    Console.WriteLine("Invalid email format. Please enter a valid email.");
+                    return;
+                }
+
+                if (!User.ValidatePhone(phone))
+                {
+                    Console.WriteLine("Invalid phone number format. Please enter a valid phone number.");
+                    return;
+                }
+
+                if (userTree.ContainsKey(phone))
+                {
+                    Console.WriteLine("A user with this phone number already exists.");
+                    return;
+                }
+
+                var user = new User(lastName, firstName, email, phone);
+                userTree.Add(phone, user);
+                Console.WriteLine("User added.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error adding user: {e.Message}");
+            }
         }
 
         static void DeleteUser()
--- User.cs
+++ User.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ContactTracker
@@ -24,6 +25,22 @@ namespace ContactTracker
         public override string ToString()
         {
             return $"{LastName}, {FirstName}, {Email}, {Phone}";
+        }
+        public static bool ValidateEmail(string email)
+        {
+            string emailPattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            return Regex.IsMatch(email, emailPattern);
+        }
+
+        public static bool ValidatePhone(string phone)
+        {
+            string phonePattern = @"^[\d\-\s]+$";
+            return Regex.IsMatch(phone, phonePattern);
+        }
+
+        public static bool ValidateName(string name)
+        {
+            return !string.IsNullOrWhiteSpace(name);
         }
     }
 }
